809d9a31a2d4b7317503bdcc95df1f403d284f57
Sameesh <sameesh.fisat@gmail.com>
2016-01-20 02:22:47 -0500

    Update fileread.py

--- fileread.py
+++ fileread.py
@@ -1,7 +1,9 @@
 with open("dataset.txt") as textFile:
-        lines = [line.split() for line in textFile]
-       print lines[0][0]
-        
-        length=lines.__len__()
-        print "Length of training set :"
-        print length
+	lines = [line.split() for line in textFile]
+	print lines[0][0]
+
+length=lines.__len__()
+print "Length of training set :"+ str(length)
+
+
+print lines[0][0]
